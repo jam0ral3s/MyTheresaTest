5671ef8a67a8c72e09258d26caee6daf6e1fcedf
Jorge Morales <jmoralesgonzalez@gmail.com>
2024-11-26 10:44:56 +0100

    Improve managing of providers

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import AppProvider from './src/AppProvider';
+import App from './src/App';
 
-AppRegistry.registerComponent(appName, () => AppProvider);
+const selectedProviders = ['DarkModeProvider', 'GenreProvider', 'ScrollPositionProvider'];
+
+const RootApp = () => {
+  return (
+    <AppProvider providers={selectedProviders}>
+      <App />
+    </AppProvider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => AppProvider({children: <RootApp/>}));
--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import App from './App';
+import React, {ReactNode} from 'react';
 import {DarkModeProvider} from './service/DarkModeProvider';
 import {GenreProvider} from './service/GenreProvider';
 import {ScrollPositionProvider} from './service/ScrollPositionProvider';
 
-const AppProvider = (): React.JSX.Element => {
-  return (
-    <DarkModeProvider>
-      <GenreProvider>
-        <ScrollPositionProvider>
-          <App />
-        </ScrollPositionProvider>
-      </GenreProvider>
-    </DarkModeProvider>
-  );
+const providers = {
+  DarkModeProvider,
+  GenreProvider,
+  ScrollPositionProvider,
+};
+
+type ProviderProps = {
+  providers: Array<keyof typeof providers>;
+  children: ReactNode;
+};
+
+const AppProvider = ({
+  providers: selectedProviders,
+  children,
+}: ProviderProps): React.JSX.Element => {
+  return selectedProviders.reduceRight((acc, providerName) => {
+    const Provider = providers[providerName];
+    return <Provider>{acc}</Provider>;
+  }, children as React.ReactElement);
 };
 
 export default AppProvider;
